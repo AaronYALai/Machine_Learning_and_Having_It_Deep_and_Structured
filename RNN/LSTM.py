c0bd356c6f885435d39f95fe4ee5499e8aa67869
AaronLai <aaronlaiya@icloud.com>
2015-11-19 22:17:14 +0800

    Correct Bugs in LSTM and RNN

--- RNN/LSTM.py
+++ RNN/LSTM.py
@@ -1,4 +1,4 @@
-#Bi-directional RNN, Momentum, NAG, RMSProp, Dropout 
+#Bi-directional Long Short Term Memory (Momentum, NAG, RMSProp, Dropout)
 import theano as th
 import theano.tensor as T
 import numpy as np
@@ -8,7 +8,7 @@ from theano.tensor.shared_randomstreams import RandomStreams
 from collections import defaultdict
 
 start_time = time.clock()
-Data = np.load('prob_t.npz') #DNN's output
+Data = np.load('prob_t.npz') #Output of DNN
 Data = Data[Data.files[0]]
 F = np.load('TrainID.npz')
 TrainID = F[F.files[0]][0]
@@ -122,67 +122,121 @@ def Valid_Y():
 
 print("Data loaded, using %f seconds"%(time.clock()-start_time))
 
-#RNN structure
+#LSTM structure
 st = time.clock()
-b = 1    #Batch size
+bat = 1 #Batch size
 learing_rate = 0.00003
 srng = RandomStreams(seed=5432)
 
 x_seq = T.fmatrix()
 y_hat = T.fmatrix()
-ind = T.scalar()    #Help to do minibatch
-bud = T.scalar()    #Help to do Dropout 
+ind = T.scalar() #Help to do minibatch
+bud = T.scalar() #Help to do Dropout 
 
-cons = 0.001; a=0.0; s=0.01; neuron = 160
+##### Initialize parameters #####
+cons = 0.001; a=0; s=0.01; neuron = 48
 a_0 = th.shared(0*np.random.randn(neuron))
-Wi = th.shared(s*np.random.randn(48,neuron))
-bi = th.shared(cons*np.random.randn(neuron)-a)
+h_0 = th.shared(0*np.random.randn(neuron))
+Wi = th.shared(s*np.random.randn(48,neuron)-a)
+Wf = th.shared(s*np.random.randn(48,neuron)+0.005-a)
+Wo = th.shared(s*np.random.randn(48,neuron)-a)
+W = th.shared(s*np.random.randn(48,neuron)-a)
+Ui = th.shared(s*np.random.randn(neuron,neuron)-a)
+Uf = th.shared(s*np.identity(neuron)-a)
+Uo = th.shared(s*np.random.randn(neuron,neuron)-a)
+U = th.shared(s*np.random.randn(neuron,neuron)-a)
+Vi = th.shared(s*np.random.randn(neuron,neuron)-a)
+Vf = th.shared(s*np.identity(neuron)-a)
+Vo = th.shared(s*np.random.randn(neuron,neuron)-a)
+bi = th.shared(cons*np.random.randn(neuron))#-a)
+bf = th.shared(cons*np.random.randn(neuron))#-a)
+bo = th.shared(cons*np.random.randn(neuron))#-a)
+b = th.shared(cons*np.random.randn(neuron))#-a)
+#Wout = th.shared(cons*np.random.randn(2*neuron,48)-a)
 
-Wh = th.shared(s*np.identity(neuron)-a)
-Wof = th.shared(s*np.random.randn(2*neuron,neuron)-a)
-Wob = th.shared(s*np.random.randn(2*neuron,neuron)-a)
-bh = th.shared(cons*np.random.randn(neuron)-a)
-bof = th.shared(cons*np.random.randn(neuron)-a)
-bob = th.shared(cons*np.random.randn(neuron)-a)
-"""
-W2h = th.shared(s*np.identity(neuron)-a)
-W2of = th.shared(s*np.random.randn(2*neuron,neuron)-a)
-W2ob = th.shared(s*np.random.randn(2*neuron,neuron)-a)
-b2h = th.shared(cons*np.random.randn(neuron)-a)
-b2of = th.shared(cons*np.random.randn(neuron)-a)
-b2ob = th.shared(cons*np.random.randn(neuron)-a)
-"""
-W3h = th.shared(s*np.identity(neuron)-a)
-W3o = th.shared(s*np.random.randn(2*neuron,48)-a)
-b3h = th.shared(cons*np.random.randn(neuron)-a)
-b3o = th.shared(cons*np.random.randn(48)-a)
+Wi1f = th.shared(s*np.random.randn(2*neuron,neuron)-a)
+Wf1f = th.shared(s*np.random.randn(2*neuron,neuron)+0.005-a)
+Wo1f = th.shared(s*np.random.randn(2*neuron,neuron)-a)
+W1f = th.shared(s*np.random.randn(2*neuron,neuron)-a)
+Wi1b = th.shared(s*np.random.randn(2*neuron,neuron)-a)
+Wf1b = th.shared(s*np.random.randn(2*neuron,neuron)+0.005-a)
+Wo1b = th.shared(s*np.random.randn(2*neuron,neuron)-a)
+W1b = th.shared(s*np.random.randn(2*neuron,neuron)-a)
+Ui1 = th.shared(s*np.random.randn(neuron,neuron)-a)
+Uf1 = th.shared(s*np.identity(neuron)-a)
+Uo1 = th.shared(s*np.random.randn(neuron,neuron)-a)
+U1 = th.shared(s*np.random.randn(neuron,neuron)-a)
+Vi1 = th.shared(s*np.random.randn(neuron,neuron)-a)
+Vf1 = th.shared(s*np.identity(neuron)-a)
+Vo1 = th.shared(s*np.random.randn(neuron,neuron)-a)
+bi1f = th.shared(cons*np.random.randn(neuron))#-a)
+bf1f = th.shared(cons*np.random.randn(neuron))#-a)
+bo1f = th.shared(cons*np.random.randn(neuron))#-a)
+b1f = th.shared(cons*np.random.randn(neuron))#-a)
+bi1b = th.shared(cons*np.random.randn(neuron))#-a)
+bf1b = th.shared(cons*np.random.randn(neuron))#-a)
+bo1b = th.shared(cons*np.random.randn(neuron))#-a)
+b1b = th.shared(cons*np.random.randn(neuron))#-a)
+W1out = th.shared(cons*np.random.randn(2*neuron,48)-a)
 
 Auxiliary = []; Temp = []
-parameters = [Wi,bi,Wh,Wof,Wob,bh,bof,bob,W3h,W3o,b3h,b3o]#W2h,W2of,W2ob,b2h,b2of,b2ob,
+parameters = [Wi,Wf,Wo,W,bi,bf,bo,b,U,Ui,Uf,Uo,Vi,Vf,Vo,Wi1f,Wf1f,Wo1f,W1f,Wi1b,Wf1b,Wo1b,W1b,bi1f,bf1f,bo1f,b1f
+              ,bi1b,bf1b,bo1b,b1b,U1,Ui1,Uf1,Uo1,Vi1,Vf1,Vo1,W1out]
 for param in parameters:
     Auxiliary.append(th.shared(np.zeros(param.get_value().shape)))
     Temp.append(th.shared(np.zeros(param.get_value().shape)))
-    
-c = 5
+
+##### Do gradient clip #####
+c = 1
 Wi = th.gradient.grad_clip(Wi,-c,c)
 bi = th.gradient.grad_clip(bi,-c,c)
-Wh = th.gradient.grad_clip(Wh,-c,c)
-Wof = th.gradient.grad_clip(Wof,-c,c)
-Wob = th.gradient.grad_clip(Wob,-c,c)
-bh = th.gradient.grad_clip(bh,-c,c)
-bof = th.gradient.grad_clip(bof,-c,c)
-bob = th.gradient.grad_clip(bob,-c,c)
-W3h = th.gradient.grad_clip(W3h,-c,c)
-W3o = th.gradient.grad_clip(W3o,-c,c)
-b3h = th.gradient.grad_clip(b3h,-c,c)
-b3o = th.gradient.grad_clip(b3o,-c,c)  
+Wf = th.gradient.grad_clip(Wf,-c,c)
+bf = th.gradient.grad_clip(bf,-c,c)
+Wo = th.gradient.grad_clip(Wo,-c,c)
+bo = th.gradient.grad_clip(bo,-c,c)
+W = th.gradient.grad_clip(W,-c,c)
+b = th.gradient.grad_clip(b,-c,c)
+Ui = th.gradient.grad_clip(Ui,-c,c)
+Uf = th.gradient.grad_clip(Uf,-c,c)
+Uo = th.gradient.grad_clip(Uo,-c,c)
+U = th.gradient.grad_clip(U,-c,c)
+Vi = th.gradient.grad_clip(Vi,-c,c)
+Vf = th.gradient.grad_clip(Vf,-c,c)
+Vo = th.gradient.grad_clip(Vo,-c,c)
+#Wout = th.gradient.grad_clip(Wout,-c,c)
 
+Wi1f = th.gradient.grad_clip(Wi1f,-c,c)
+bi1f = th.gradient.grad_clip(bi1f,-c,c)
+Wf1f = th.gradient.grad_clip(Wf1f,-c,c)
+bf1f = th.gradient.grad_clip(bf1f,-c,c)
+Wo1f = th.gradient.grad_clip(Wo1f,-c,c)
+bo1f = th.gradient.grad_clip(bo1f,-c,c)
+W1f = th.gradient.grad_clip(W1f,-c,c)
+b1f = th.gradient.grad_clip(b1f,-c,c)
+Wi1b = th.gradient.grad_clip(Wi1b,-c,c)
+bi1b = th.gradient.grad_clip(bi1b,-c,c)
+Wf1b = th.gradient.grad_clip(Wf1b,-c,c)
+bf1b = th.gradient.grad_clip(bf1b,-c,c)
+Wo1b = th.gradient.grad_clip(Wo1b,-c,c)
+bo1b = th.gradient.grad_clip(bo1b,-c,c)
+W1b = th.gradient.grad_clip(W1b,-c,c)
+b1b = th.gradient.grad_clip(b1b,-c,c)
+Ui1 = th.gradient.grad_clip(Ui1,-c,c)
+Uf1 = th.gradient.grad_clip(Uf1,-c,c)
+Uo1 = th.gradient.grad_clip(Uo1,-c,c)
+U1 = th.gradient.grad_clip(U1,-c,c)
+Vi1 = th.gradient.grad_clip(Vi1,-c,c)
+Vf1 = th.gradient.grad_clip(Vf1,-c,c)
+Vo1 = th.gradient.grad_clip(Vo1,-c,c)
+W1out = th.gradient.grad_clip(W1out,-c,c)
+
+##### Optimization methods #####
 def Update_Momentum(para,grad,ind,Momentum,Temp):
     """theano update, optimized by Momentum"""
-    updates = []; off_on = ifelse(T.lt(ind,b-1),0,1)
+    updates = []; off_on = ifelse(T.lt(ind,bat-1),0,1)
     for ix in range(len(grad)):
-        #gradient = T.clip(grad[ix],-1,1)
-        direction = (0.95)*Momentum[ix] - (learing_rate/b)*(gradient+Temp[ix])
+        gradient = T.clip(grad[ix],-1,1)#T.clip(grad[ix],np.float32(-0.01),np.float32(0.01))
+        direction = (0.95)*Momentum[ix] - (learing_rate/bat)*(gradient+Temp[ix])
         updates.append((para[ix], para[ix]+direction*off_on))
         updates.append((Momentum[ix], Momentum[ix]*(1-off_on)+direction*off_on))
         updates.append((Temp[ix], (Temp[ix]+gradient)*(1-off_on)))
@@ -190,10 +244,10 @@ def Update_Momentum(para,grad,ind,Momentum,Temp):
 
 def Update_NAG(para,grad,ind,Real,Temp):
     """theano update, optimized by NAG"""
-    updates = []; off_on = ifelse(T.lt(ind,b-1),0,1)
+    updates = []; off_on = ifelse(T.lt(ind,bat-1),0,1)
     for ix in range(len(grad)):
-        #grad[ix] = T.clip(grad[ix],-1,1)
-        gradient = -(learing_rate/b)*(grad[ix]+Temp[ix])
+        grad[ix] = T.clip(grad[ix],-1,1)
+        gradient = -(learing_rate/bat)*(grad[ix]+Temp[ix])
         spy_position = (1+0.95)*(para[ix]+gradient)-0.95*Real[ix]
         updates.append((para[ix], (spy_position)*off_on+para[ix]*(1-off_on)))
         updates.append((Real[ix], (para[ix]+gradient)*off_on+Real[ix]*(1-off_on)))
@@ -202,81 +256,97 @@ def Update_NAG(para,grad,ind,Real,Temp):
 
 def Update_RMSProp(para,grad,ind,Sigma_square,Temp):
     """theano update, optimized by RMSProp"""
-    updates = []; off_on = ifelse(T.lt(ind,b-1),0,1); alpha = 0.9
+    updates = []; off_on = ifelse(T.lt(ind,bat-1),0,1); alpha = 0.9
     for ix in range(len(grad)):
-        grad[ix] = T.clip(grad[ix],-1,1)
-        gradient = (grad[ix]+Temp[ix])/b
-        Factor = Sigma_square[ix]*alpha+(1-alpha)*(gradient**2)
-        direction = -(learing_rate)*gradient/(T.sqrt(Factor)+0.001)
+        #grad[ix] = T.clip(grad[ix],-1,1)
+        gradient = (grad[ix]+Temp[ix])/bat
+        Factor = Sigma_square[ix]*alpha+(1-alpha)*gradient**2
+        direction = -(learing_rate)*gradient/T.sqrt(Factor)
         updates.append((para[ix], (para[ix]+direction)*off_on+para[ix]*(1-off_on)))
         updates.append((Sigma_square[ix], Factor*off_on+Sigma_square[ix]*(1-off_on)))
         updates.append((Temp[ix], (Temp[ix]+grad[ix])*(1-off_on)))
     return updates
 
+##### Activation Functions #####
 def sigmoid(Z):
     return 1/(1+T.exp(-Z))
 
 def ReLU(Z):
     return T.switch(Z<0,0,Z)
 
+def tanh(Z):
+    return T.tanh(Z)
+
 def softmax(Z):
     z = T.exp(Z)
     return (z.T/T.sum(z,axis=1)).T
 
-def step(zf_t,zb_t,af_tm1,ab_tm1):
-    af_t = ReLU( zf_t + T.dot(af_tm1,Wh) + bh )
-    ab_t = ReLU( zb_t + T.dot(ab_tm1,Wh) + bh )
-    return af_t, ab_t
+def step(z_t,zi_t,zf_t,zo_t,c_tm1,h_tm1):
+    Z_t = tanh(z_t + T.dot(h_tm1,U))
+    Zi_t = sigmoid(zi_t + T.dot(h_tm1,Ui) + T.dot(c_tm1,Vi))
+    Zf_t = sigmoid(zf_t + T.dot(h_tm1,Uf) + T.dot(c_tm1,Vf)) 
+    c_t = Z_t*Zi_t + c_tm1*Zf_t
+    Zo_t = sigmoid(zo_t + T.dot(h_tm1,Uo) + T.dot(c_t,Vo))
+    h_t = tanh(c_t)*Zo_t
+    return c_t, h_t
 
-def step2(zf_t,zb_t,af_tm1,ab_tm1):
-    af_t = ReLU( zf_t + T.dot(af_tm1,W2h) + b2h )
-    ab_t = ReLU( zb_t + T.dot(ab_tm1,W2h) + b2h )
-    return af_t, ab_t
+def step2(z_t,zi_t,zf_t,zo_t,c_tm1,h_tm1):
+    Z_t = tanh(z_t + T.dot(h_tm1,U1))
+    Zi_t = sigmoid(zi_t + T.dot(h_tm1,Ui1) + T.dot(c_tm1,Vi1))
+    Zf_t = sigmoid(zf_t + T.dot(h_tm1,Uf1) + T.dot(c_tm1,Vf1)) 
+    c_t = Z_t*Zi_t + c_tm1*Zf_t
+    Zo_t = sigmoid(zo_t + T.dot(h_tm1,Uo1) + T.dot(c_t,Vo1))
+    h_t = tanh(c_t)*Zo_t
+    return c_t, h_t
 
-def step3(zf_t,zb_t,af_tm1,ab_tm1):
-    af_t = ReLU( zf_t + T.dot(af_tm1,W3h) + b3h )
-    ab_t = ReLU( zb_t + T.dot(ab_tm1,W3h) + b3h )
-    return af_t, ab_t
+##### Structure Building #####
+z_seq = T.dot(x_seq,W)+b.dimshuffle('x',0)
+zi_seq = T.dot(x_seq,Wi)+bi.dimshuffle('x',0)
+zo_seq = T.dot(x_seq,Wo)+bo.dimshuffle('x',0)
+zf_seq = T.dot(x_seq,Wf)+bf.dimshuffle('x',0)
 
-##### Layer 1 ######
-z_seq = T.dot(x_seq,Wi)+bi.dimshuffle('x',0)
-[af_seq,ab_seq],_ = th.scan(step, sequences = [z_seq,z_seq[::-1]], 
-                               outputs_info = [a_0,a_0],
+# Forward direction
+[cf_seq,hf_seq],_ = th.scan(step, sequences = [z_seq,zi_seq,zf_seq,zo_seq], 
+                               outputs_info = [a_0,h_0],
                               truncate_gradient=-1)
-a_out = T.concatenate([af_seq,ab_seq[::-1]],axis=1)
-a_seq = ifelse(T.lt(bud,1.05), a_out*srng.binomial(size=T.shape(a_out),p=0.8),a_out)/bud
-
-##### Layer 2 ######
-z1_f = T.dot(a_seq,Wof)+bof.dimshuffle('x',0)
-z1_b = T.dot(a_seq,Wob)+bob.dimshuffle('x',0)
-[a2f_seq,a2b_seq],_ = th.scan(step3, sequences = [z1_f,z1_b[::-1]], 
-                               outputs_info = [a_0,a_0],
+# Backward direction
+[cb_seq,hb_seq],_ = th.scan(step, sequences = [z_seq[::-1],zi_seq[::-1],zf_seq[::-1],zo_seq[::-1]], 
+                               outputs_info = [a_0,h_0],
                               truncate_gradient=-1)
-"""
-a2_seq = T.concatenate([a2f_seq,a2b_seq[::-1]],axis=1)
 
-##### Layer 3 ######
-z2_f = T.dot(a2_seq,W2of)+b2of.dimshuffle('x',0)
-z2_b = T.dot(a2_seq,W2ob)+b2ob.dimshuffle('x',0)
+y1_seq = T.concatenate([hf_seq,hb_seq[::-1]],axis=1)
+#h_seq = ifelse(T.lt(bud,1.05), h_out*srng.binomial(size=T.shape(h_out),p=0.8),h_out)/bud  #Dropout
+#y1_seq = softmax(T.dot(h_out,Wout))*(bud/bud)
 
-[a3f_seq,a3b_seq],_ = th.scan(step3, sequences = [z2_f,z2_b[::-1]], 
-                               outputs_info = [a_0,a_0],
+z1f_seq = T.dot(y1_seq,W1f)+b1f.dimshuffle('x',0)
+zi1f_seq = T.dot(y1_seq,Wi1f)+bi1f.dimshuffle('x',0)
+zo1f_seq = T.dot(y1_seq,Wo1f)+bo1f.dimshuffle('x',0)
+zf1f_seq = T.dot(y1_seq,Wf1f)+bf1f.dimshuffle('x',0)
+
+z1b_seq = T.dot(y1_seq,W1b)+b1b.dimshuffle('x',0)
+zi1b_seq = T.dot(y1_seq,Wi1b)+bi1b.dimshuffle('x',0)
+zo1b_seq = T.dot(y1_seq,Wo1b)+bo1b.dimshuffle('x',0)
+zf1b_seq = T.dot(y1_seq,Wf1b)+bf1b.dimshuffle('x',0)
+
+# Forward direction
+[cf1_seq,hf1_seq],_ = th.scan(step2, sequences = [z1f_seq,zi1f_seq,zf1f_seq,zo1f_seq], 
+                               outputs_info = [a_0,h_0],
                               truncate_gradient=-1)
-"""
+# Backward direction
+[cb1_seq,hb1_seq],_ = th.scan(step2, sequences = [z1b_seq[::-1],zi1b_seq[::-1],zf1b_seq[::-1],zo1b_seq[::-1]], 
+                               outputs_info = [a_0,h_0],
+                              truncate_gradient=-1)
 
-a3_out = T.concatenate([a2f_seq,a2b_seq[::-1]],axis=1)
-a3_seq = ifelse(T.lt(bud,1.05), a3_out*srng.binomial(size=T.shape(a3_out),p=0.8),a3_out)/bud
-y3_pre = T.dot(a3_seq,W3o)+b3o.dimshuffle('x',0)
-
-y_seq = softmax(y3_pre)
+h1_out = T.concatenate([hf1_seq,hb1_seq[::-1]],axis=1)
+y_seq = softmax(T.dot(h1_out,W1out))+bud*0
 forword = th.function(inputs=[x_seq,bud],outputs=y_seq)
 
-cost = (1+ind-ind)*T.sum((y_seq-y_hat)**2)  #create variable dependency for "ind" variable(theano requirement)
+cost = (1+ind-ind)*T.sum((y_seq-y_hat)**2)
 valid = th.function(inputs=[x_seq,y_hat,ind,bud],outputs=cost)
-grads = T.grad(cost,parameters,disconnected_inputs='ignore')
-            
+grads = T.grad(cost,parameters)#,disconnected_inputs='ignore')
+    
 rnn_train = th.function(inputs=[x_seq,y_hat,ind,bud],outputs=cost,
-                       updates=Update_NAG(parameters,grads,ind,Auxiliary,Temp)) #Optimized by NAG
+                       updates=Update_NAG(parameters,grads,ind,Auxiliary,Temp))
 
 def Validation():
     """Calculate the average edit distance on validation set"""
@@ -291,7 +361,7 @@ def Validation():
         Valid_X.append(xval[:-1])
         
     Valid_Yhat = Valid_Y()
-    return np.mean([edit(Valid_X[i] ,Valid_Yhat[i]) for i in range(len(Valid_X))])
+    return np.mean([edit(Valid_X[i] ,Valid_Yhat[i]) for i in range(len(Valid_X))]).astype(np.float32)
 
 def Valid_Cost():
     """Calculate the cost of the validation set"""
@@ -329,10 +399,11 @@ for j in range(5):
                 #if len(Record)>1 and Record[-2]+0.5 <= Record[-1]:
                 #    break
 
-        batch = batch+1 if batch != (b-1) else 0
+        batch = batch+1 if batch != (bat-1) else 0
     print('%d Epoch(s) trained, %f seconds passed from start.'%((j+1),time.clock()-st))
 
-print("Done training. Using %f seconds." % (time.clock()-st))
+end = time.clock()
+print("Done training. Using %f seconds." % (end-st))
 
 print("Start predicting...")
 
@@ -349,7 +420,7 @@ for i in range(len(Test_Num)):
         pre.append(mapping[Map_label(np.argmax(l))])
     Prediction.append(pre)
 
-def check(L,i):
+def check(L,i): 
     """Sanity Check function to correct unreasonable predictions"""
     if L[i-1] == L[i+1] and L[i] != L[i-1]: #front == behind != me
         return True
@@ -362,10 +433,10 @@ for inde, pred in enumerate(Prediction):
     for i in range(1,len(pred)-1):
         if check(pred,i):
             pred[i] = pred[i-1]
-    Prediction[j] = pred
+    Prediction[inde] = pred
 
 import csv
-csvfile = open('Vprediction.csv','w',newline='')
+csvfile = open('LSTM_predict.csv','w',newline='')
 write = csv.writer(csvfile, delimiter=',')
 write.writerow(['id','phone_sequence'])
 
